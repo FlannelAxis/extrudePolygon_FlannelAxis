5551061cb0904c44d097a79d0578e261a0743d87
ethan <ethan@ethan-Laptop-12th-Gen-Intel-Core>
2023-06-03 15:19:21 -0400

    Vector Points
    To learn how the features work

--- extrudePolygon.groovy
+++ extrudePolygon.groovy
@@ -1,9 +1,9 @@
 
 //create an extruded polygon
 CSG polygon = Extrude.points(new Vector3d(0, 0, 40),// This is the  extrusion depth
-                new Vector3d(0,0),// All values after this are the points in the polygon
+                new Vector3d(7,23,3),// All values after this are the points in the polygon
                 new Vector3d(80,0),// Bottom right corner
-                new Vector3d(60,40),// upper right corner
-                new Vector3d(20,40)// upper left corner
+                new Vector3d(60,50),// upper right corner
+                new Vector3d(20,40,-5)// upper left corner
         );
 return polygon
